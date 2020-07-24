7d3dd85c2cc51862b95398766013bbd284b8b133
KIM DAE HWI <dhxl50@gmail.com>
2020-07-24 23:43:42 +0900

    mysqlDB 연결

--- server.js
+++ server.js
@@ -1,8 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const xlsx = require('xlsx');
+const fs = require("fs");
+const mysql = require("mysql");
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+console.log(conf);
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+
+function handleDisconnect() {
+  connection.connect(function (err) {
+    if (err) {
+      console.log("error when connecting to connection:", err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+
+  connection.on("error", function (err) {
+    console.log("connection error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      return handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
