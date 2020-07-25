c2d8c17597892680bf0691716ebb2d3702448330
swa07016 <swa07016@naver.com>
2020-07-25 12:22:37 +0900

    Donate 등록 모달 완성

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import CList from './CList';
-
+import Rbtns from './Rbtns';
 ReactDOM.render(<App />, document.getElementById('donate-list'));
 ReactDOM.render(<CList />, document.getElementById('covid-list'));
+ReactDOM.render(<Rbtns/>, document.getElementById('register-btns'));
